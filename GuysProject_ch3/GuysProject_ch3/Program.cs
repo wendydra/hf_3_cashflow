e425f5ed50b4e06fabc6fc6cea17a119f16e5027
Wendy Dang <wdang@mimeo.com>
2016-12-29 09:00:08 -0800

    Adds GiveCash and ReceiveCash methods for passing and reducing cash

--- GuysProject_ch3/GuysProject_ch3/Program.cs
+++ GuysProject_ch3/GuysProject_ch3/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace GuysProject_ch3
@@ -17,6 +14,43 @@ namespace GuysProject_ch3
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+        }
+
+        class Guy
+        {
+            public string Name;
+            public int Cash;
+
+            public int GiveCash(int amount)
+            {
+                //You are issuing a command at a guy to give cash, which simply decrements his amount. 
+                //At this point you are not passing that amount to the other guy yet. 
+                if (amount <= Cash && amount > 0)
+                {
+                    Cash -= amount;
+                    return amount; 
+                }
+                else
+                {
+                    MessageBox.Show(
+                        "Not enough cash " + amount, Name + " says.");
+                    return 0;
+                }
+            }
+            public int ReceiveCash(int amount)
+            {
+                if (amount > 0)
+                {
+                    Cash += amount;
+                    return amount;
+                }
+                else
+                {
+                    MessageBox.Show(
+                        "That's negative!" + amount);
+                    return 0;
+                }
+            }
         }
     }
 }
