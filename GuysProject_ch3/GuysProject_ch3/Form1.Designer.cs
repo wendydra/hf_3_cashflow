59f772af9f3b86c789b0526d9e2e3493f71617b7
Wendy Dang <wdang@mimeo.com>
2016-12-29 09:07:15 -0800

    Corrects class problems

--- GuysProject_ch3/GuysProject_ch3/Form1.Designer.cs
+++ GuysProject_ch3/GuysProject_ch3/Form1.Designer.cs
@@ -28,12 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.joeButton = new System.Windows.Forms.Button();
+            this.bobButton = new System.Windows.Forms.Button();
+            this.joesCashLabel = new System.Windows.Forms.Label();
+            this.bobsCashLabel = new System.Windows.Forms.Label();
+            this.bankCashLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // joeButton
+            // 
+            this.joeButton.Location = new System.Drawing.Point(40, 171);
+            this.joeButton.Name = "joeButton";
+            this.joeButton.Size = new System.Drawing.Size(75, 38);
+            this.joeButton.TabIndex = 0;
+            this.joeButton.Text = "Give $10 to Joe";
+            this.joeButton.UseVisualStyleBackColor = true;
+            // 
+            // bobButton
+            // 
+            this.bobButton.Location = new System.Drawing.Point(172, 171);
+            this.bobButton.Name = "bobButton";
+            this.bobButton.Size = new System.Drawing.Size(76, 38);
+            this.bobButton.TabIndex = 1;
+            this.bobButton.Text = "Receive $5 from Bob";
+            this.bobButton.UseVisualStyleBackColor = true;
+            // 
+            // joesCashLabel
+            // 
+            this.joesCashLabel.AutoSize = true;
+            this.joesCashLabel.Location = new System.Drawing.Point(40, 33);
+            this.joesCashLabel.Name = "joesCashLabel";
+            this.joesCashLabel.Size = new System.Drawing.Size(35, 13);
+            this.joesCashLabel.TabIndex = 2;
+            this.joesCashLabel.Text = "label1";
+            // 
+            // bobsCashLabel
+            // 
+            this.bobsCashLabel.AutoSize = true;
+            this.bobsCashLabel.Location = new System.Drawing.Point(40, 72);
+            this.bobsCashLabel.Name = "bobsCashLabel";
+            this.bobsCashLabel.Size = new System.Drawing.Size(35, 13);
+            this.bobsCashLabel.TabIndex = 3;
+            this.bobsCashLabel.Text = "label2";
+            // 
+            // bankCashLabel
+            // 
+            this.bankCashLabel.AutoSize = true;
+            this.bankCashLabel.Location = new System.Drawing.Point(40, 112);
+            this.bankCashLabel.Name = "bankCashLabel";
+            this.bankCashLabel.Size = new System.Drawing.Size(35, 13);
+            this.bankCashLabel.TabIndex = 4;
+            this.bankCashLabel.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.bankCashLabel);
+            this.Controls.Add(this.bobsCashLabel);
+            this.Controls.Add(this.joesCashLabel);
+            this.Controls.Add(this.bobButton);
+            this.Controls.Add(this.joeButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button joeButton;
+        private System.Windows.Forms.Button bobButton;
+        private System.Windows.Forms.Label joesCashLabel;
+        private System.Windows.Forms.Label bobsCashLabel;
+        private System.Windows.Forms.Label bankCashLabel;
     }
 }
 
---
+++ GuysProject_ch3/GuysProject_ch3/Guy.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows.Forms;
+
+namespace GuysProject_ch3
+{
+    class Guy
+    {
+        public string Name;
+        public int Cash;
+
+        public int GiveCash(int amount)
+        {
+            //You are issuing a command at a guy to give cash, which simply decrements his amount. 
+            //At this point you are not passing that amount to the other guy yet. 
+            if (amount <= Cash && amount > 0)
+            {
+                Cash -= amount;
+                return amount;
+            }
+            else
+            {
+                MessageBox.Show(
+                    "Not enough cash " + amount, Name + " says.");
+                return 0;
+            }
+        }
+        public int ReceiveCash(int amount)
+        {
+            if (amount > 0)
+            {
+                Cash += amount;
+                return amount;
+            }
+            else
+            {
+                MessageBox.Show(
+                    "That's negative!" + amount);
+                return 0;
+            }
+        }
+    }
+}
